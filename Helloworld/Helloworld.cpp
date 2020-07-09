dad62ea8ed6c62fba5f343a62f738d4bc2828c21
engsoftwill <engeletwill@gmail.com>
2020-07-09 15:10:32 -0300

    inclusao primeiro projeto HelloWorld

---
+++ Helloworld/Helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// posso importasr todos os identificadores da biblioteca utilizando  using namespace std;
+
+int main(){
+    std::cout<<"Hello World"<< std::endl; // cout é funcao de saida do c++ endl pula linha
+    return 0; //iostream importa o identificadores de entrada e saída como cin, cout e endl
+    }
