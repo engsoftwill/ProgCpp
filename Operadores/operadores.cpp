5a85465b8656fa1b36d1f6d4d8dc9676cbda158f
engsoftwill <engeletwill@gmail.com>
2020-07-11 15:28:18 -0300

    add operadores.cpp

---
+++ Operadores/operadores.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int var1,var2, resultado;
+    var1=10;
+    var2=5;
+    int n=0;
+    n +=1; //n++
+    n -=1; //n--
+    n *=2; //n=n*2
+    n /=2; //n=n/2
+
+
+    cout<< var1 <<endl << var2 <<endl << var1+var2 <<endl << var1-var2<<endl << var1*var2<<endl << (float)var1/var2 <<endl;
+   return 0;
+    }
